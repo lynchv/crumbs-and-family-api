696a553444751a376a641a718bdcd19ac6cc93b2
Vincent Lynch <vincent.lynch70@gmail.com>
2019-12-21 09:43:36 -0500

    Initial structure done

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
-from FlaskApp.routes import api
 from flask_sqlalchemy import SQLAlchemy
 
+# Create  basic flask app with required config
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-api.init_app(app)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://test.db'
+# Create a flask-sqlalchemy hook
 db = SQLAlchemy(app)
 
+# Register the restplus API
+from FlaskApp.routes import api
+api.init_app(app, doc='/api')
--- FlaskApp/routes/item.py
+++ FlaskApp/routes/item.py
@@ -1,10 +1,12 @@
-from flask_restplus import Namespace
+from flask_restplus import Namespace, Resource
 from FlaskApp.models.item import Item
+
 api = Namespace('cats', description='Cats related operations')
 
 
 @api.route('/')
 class CatList(Resource):
     def get(self):
+        print("here")
         print(Item.query.all())
         return "test"
